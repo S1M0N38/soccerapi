463bd3d21c4035daed49e4844e58be31ee88b11b
S1M0N38 <bertolottosimone@gmail.com>
2020-01-11 17:43:00 +0100

    odds, and parse various bet type

--- api/888sport.py
+++ api/888sport.py
@@ -1,6 +1,8 @@
 import json
 import os
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
+
+import httpx
 
 
 class Api888Sport:
@@ -31,7 +33,7 @@ class Api888Sport:
             )
             raise KeyError(msg)
         try:
-            league = self.table[country]['leagues'][league]
+            league = self.table[country]['leagues'][league]['name']
         except KeyError:
             msg = (
                 f'{league} is not in {self.name} table. '
@@ -39,3 +41,94 @@ class Api888Sport:
             )
             raise KeyError(msg)
         return country, league
+
+    @staticmethod
+    def _full_time_result_under_over(data: Dict) -> List:
+        """ Parse the raw json requests for full_time_result & under_over """
+
+        odds = []
+        for event in data['events']:
+            if event['event']['state'] == 'STARTED':
+                continue
+            odds.append(
+                {
+                    'time': event['event']['start'],
+                    'home_team': event['event']['homeName'],
+                    'away_team': event['event']['awayName'],
+                    'full_time_resut': {
+                        '1': event['betOffers'][0]['outcomes'][0]['odds'],
+                        'X': event['betOffers'][0]['outcomes'][1]['odds'],
+                        '2': event['betOffers'][0]['outcomes'][2]['odds'],
+                    },
+                    'under_over': {
+                        'under': event['betOffers'][1]['outcomes'][1]['odds'],
+                        'over': event['betOffers'][1]['outcomes'][0]['odds'],
+                    },
+                }
+            )
+        return odds
+
+    @staticmethod
+    def _both_teams_to_score(data: Dict) -> List:
+        """ Parse the raw json requests for both_teams_to_score """
+
+        odds = []
+        for event in data['events']:
+            if event['event']['state'] == 'STARTED':
+                continue
+            odds.append(
+                {
+                    'time': event['event']['start'],
+                    'home_team': event['event']['homeName'],
+                    'away_team': event['event']['awayName'],
+                    'both_teams_to_score': {
+                        'yes': event['betOffers'][0]['outcomes'][0]['odds'],
+                        'no': event['betOffers'][0]['outcomes'][1]['odds'],
+                    },
+                }
+            )
+        return odds
+
+    @staticmethod
+    def _double_chance(data: Dict) -> List:
+        """ Parse the raw json requests for double chance """
+
+        odds = []
+        for event in data['events']:
+            if event['event']['state'] == 'STARTED':
+                continue
+            odds.append(
+                {
+                    'time': event['event']['start'],
+                    'home_team': event['event']['homeName'],
+                    'away_team': event['event']['awayName'],
+                    'double_chance': {
+                        '1X': event['betOffers'][0]['outcomes'][0]['odds'],
+                        '12': event['betOffers'][0]['outcomes'][1]['odds'],
+                        '2X': event['betOffers'][0]['outcomes'][2]['odds'],
+                    },
+                }
+            )
+        return odds
+
+    def odds(self, country: str, league: str, market: str = 'US') -> Dict:
+        """ Get the odds for the contry-league competion as a python dict """
+
+        base_params = {'lang': 'en_US', 'market': market}
+        base_url = 'https://eu-offering.kambicdn.org/offering/v2018/888it/listView/football'
+        country, league = self._country_league(country, league)
+        url = '/'.join([base_url, country, league]) + '.json'
+
+        with httpx.Client(params=base_params) as client:
+            odds = [
+                self._full_time_result_under_over(
+                    client.get(url, params={'useCombined': True}).json()
+                ),
+                self._both_teams_to_score(
+                    client.get(url, params={'category': 11942}).json()
+                ),
+                self._double_chance(
+                    client.get(url, params={'category': 12220}).json()
+                ),
+            ]
+        return [{**i, **j, **k} for i, j, k in zip(*odds)]
