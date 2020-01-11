2336b286f75800881e67df3c38a232077b35267d
S1M0N38 <bertolottosimone@gmail.com>
2020-01-11 17:42:23 +0100

    httpx dependency

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    # 'requests', 'maya', 'records',
+    'httpx'
 ]
 
 # What packages are optional?
