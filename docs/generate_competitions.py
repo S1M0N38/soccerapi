41c7d271564d65646d055c7a9e72f82eceff0a85
S1M0N38 <bertolottosimone@gmail.com>
2020-03-11 17:41:15 +0100

    update docs for unibet bookmaker

--- docs/generate_competitions.py
+++ docs/generate_competitions.py
@@ -73,5 +73,48 @@ for country, value in table_888sport.items():
 
 s += '\n------------------------------\n'
 
+# unibet
+s += '# [ApiUnibet](http://unibet.com/)'
+
+s += (
+    '\n?> accessible from: '
+    ':us: '
+    ':canada: '
+    ':australia: '
+    ':brazil: '
+    ':switzerland: '
+    ':it: '
+    ':de: '
+    ':denmark: '
+    ':es: '
+    ':finland: '
+    ':jp:'
+    ':netherlands: '
+    ':norway: '
+    ':sweden: '
+    ':ireland: '
+    ':india: '
+    ':singapore: '
+    ':hong_kong: '
+    ':new_zealand: '
+    ':mexico: '
+    ':romania: '
+    '\n'
+)
+
+s += '\n?> inaccessible from: ' ':fr:' ':uk:' '\n'
+
+table_888sport = table('888sport')
+
+old_country = ''
+for country, value in table_888sport.items():
+    if old_country != country:
+        s += f'\n## {country}\n'
+        old_country = country
+    for league, value in value['leagues'].items():
+        s += f'- [{league}](https://s5.sir.sportradar.com/unibet/en/1/season/{value["id"]})\n'
+
+s += '\n------------------------------\n'
+
 with open('competitions.md', 'w') as f:
     f.write(s)
