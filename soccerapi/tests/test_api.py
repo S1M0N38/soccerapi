955be2b1f83970fcbd90ed87df328f1a03f2c767
S1M0N38 <bertolottosimone@gmail.com>
2020-10-09 16:34:57 +0200

    revome test competition, migration to url for bet365

--- soccerapi/tests/test_api.py
+++ soccerapi/tests/test_api.py
@@ -1,6 +1,7 @@
 import abc
 import csv
 from typing import List, Tuple
+from pprint import pprint
 
 import pytest
 import requests
@@ -8,21 +9,31 @@ import requests
 from soccerapi.api import Api888Sport, ApiBet365, ApiUnibet
 
 
-def competitions(name: str) -> List[Tuple[str, str]]:
-    competitions = []
-    url = (
-        'https://docs.google.com/spreadsheets/d/'
-        '1kHFeE1hsiCwzLBNe2gokCOfVDSocc0mcKTF3HEhQ3ec/'
-        'export?format=csv&'
-        'id=1kHFeE1hsiCwzLBNe2gokCOfVDSocc0mcKTF3HEhQ3ec&'
-        'gid=1816911805'
-    )
-    data = requests.get(url).text.splitlines()
-    rows = csv.DictReader(data)
-    for row in rows:
-        if row[name] != '' and row['country'] != 'test_country':
-            competitions.append((row['country'], row['league']))
-    return competitions
+# def competitions(name: str) -> List[Tuple[str, str]]:
+#     competitions = []
+#     url = (
+#         'https://docs.google.com/spreadsheets/d/'
+#         '1kHFeE1hsiCwzLBNe2gokCOfVDSocc0mcKTF3HEhQ3ec/'
+#         'export?format=csv&'
+#         'id=1kHFeE1hsiCwzLBNe2gokCOfVDSocc0mcKTF3HEhQ3ec&'
+#         'gid=1816911805'
+#     )
+#     data = requests.get(url).text.splitlines()
+#     rows = csv.DictReader(data)
+#     for row in rows:
+#         if row[name] != '' and row['country'] != 'test_country':
+#             competitions.append((row['country'], row['league']))
+#     return competitions
+
+
+urls_bet365 = [
+    'https://www.bet365.it/#/AC/B1/C1/D13/E52224631/F2/',  # italy-seria_a
+    'https://www.bet365.it/#/AC/B1/C1/D13/E52547961/F2/I1/',  # italy-serie-b
+    'https://www.bet365.it/#/AC/B1/C1/D13/E51761579/F2/',  # england-premier_league
+    'https://www.bet365.it/#/AC/B1/C1/D13/E51791071/F2/I1/',  # england-championship
+    'https://www.bet365.it/#/AC/B1/C1/D13/E51669667/F2/I1/',  # germany-bundesliga
+    'https://www.bet365.it/#/AC/B1/C1/D13/E51669781/F2/I1/',  # germany-bundesliga2
+]
 
 
 class BaseTest(abc.ABC):
@@ -30,31 +41,21 @@ class BaseTest(abc.ABC):
     def api(self):
         pass
 
-    def test_wrong_country(self, api):
-        with pytest.raises(KeyError, match='.*not supported'):
-            api._competition('fake_country', 'test_league')
-
-    def test_wrong_league(self, api):
-        with pytest.raises(KeyError, match='.*not supported'):
-            api._competition('test_country', 'fake_league')
-
-    def test_right_country_league(self, api):
-        competition = api._competition('test_country', 'test_league')
-        assert competition
+    # TODO add test parse_url
 
 
-@pytest.mark.Api888Sport
-class TestApi888Sport(BaseTest):
-    name = '888sport'
-
-    @pytest.fixture(scope='module')
-    def api(self):
-        yield Api888Sport()
-
-    @pytest.mark.parametrize('country,league', competitions(name))
-    def test_odds(self, api, country, league):
-        odds = api.odds(country, league)
-        assert odds
+# @pytest.mark.Api888Sport
+# class TestApi888Sport(BaseTest):
+#     name = '888sport'
+#
+#     @pytest.fixture(scope='module')
+#     def api(self):
+#         yield Api888Sport()
+#
+#     @pytest.mark.parametrize('country,league', competitions(name))
+#     def test_odds(self, api, country, league):
+#         odds = api.odds(country, league)
+#         assert odds
 
 
 @pytest.mark.ApiBet365
@@ -65,21 +66,21 @@ class TestApiBet365(BaseTest):
     def api(self):
         yield ApiBet365()
 
-    @pytest.mark.parametrize('country,league', competitions(name))
-    def test_odds(self, api, country, league):
-        odds = api.odds(country, league)
+    @pytest.mark.parametrize('url', urls_bet365)
+    def test_odds(self, api, url):
+        odds = api.odds(url)
         assert odds
 
 
-@pytest.mark.ApiUnibet
-class TestApiUnibet(BaseTest):
-    name = 'unibet'
-
-    @pytest.fixture(scope='module')
-    def api(self):
-        yield ApiUnibet()
-
-    @pytest.mark.parametrize('country,league', competitions(name))
-    def test_odds(self, api, country, league):
-        odds = api.odds(country, league)
-        assert odds
+# @pytest.mark.ApiUnibet
+# class TestApiUnibet(BaseTest):
+#     name = 'unibet'
+#
+#     @pytest.fixture(scope='module')
+#     def api(self):
+#         yield ApiUnibet()
+#
+#     @pytest.mark.parametrize('country,league', competitions(name))
+#     def test_odds(self, api, country, league):
+#         odds = api.odds(country, league)
+#         assert odds
