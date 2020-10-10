4527267f92f23ee2fc44fd7256a0722c0f667469
S1M0N38 <bertolottosimone@gmail.com>
2020-10-10 18:24:09 +0200

    test odds from url (bet365 - 888sport)

--- soccerapi/tests/test_api.py
+++ soccerapi/tests/test_api.py
@@ -1,12 +1,12 @@
 import abc
-import csv
-from typing import List, Tuple
+# import csv
 from pprint import pprint
 
 import pytest
-import requests
-
+# import requests
 from soccerapi.api import Api888Sport, ApiBet365, ApiUnibet
+
+# from typing import List, Tuple
 
 
 # def competitions(name: str) -> List[Tuple[str, str]]:
@@ -35,27 +35,37 @@ urls_bet365 = [
     'https://www.bet365.it/#/AC/B1/C1/D13/E51669781/F2/I1/',  # germany-bundesliga2
 ]
 
+urls_888sport = [
+    'https://www.888sport.it/#/filter/football/italy/serie_a',  # italy-seria_a
+    'https://www.888sport.it/#/filter/football/italy/serie_b',  # italy-serie-b
+    'https://www.888sport.it/#/filter/football/england/premier_league',  # england-premier_league
+    'https://www.888sport.it/#/filter/football/england/the_championship',  # england-championship
+    'https://www.888sport.it/#/filter/football/germany/bundesliga',  # germany-bundesliga
+    'https://www.888sport.it/#/filter/football/germany/2__bundesliga',  # germany-bundesliga2
+]
+
 
 class BaseTest(abc.ABC):
     @abc.abstractmethod
     def api(self):
         pass
 
-    # TODO add test parse_url
 
+@pytest.mark.Api888Sport
+class TestApi888Sport(BaseTest):
+    name = '888sport'
 
-# @pytest.mark.Api888Sport
-# class TestApi888Sport(BaseTest):
-#     name = '888sport'
-#
-#     @pytest.fixture(scope='module')
-#     def api(self):
-#         yield Api888Sport()
-#
-#     @pytest.mark.parametrize('country,league', competitions(name))
-#     def test_odds(self, api, country, league):
-#         odds = api.odds(country, league)
-#         assert odds
+    @pytest.fixture(scope='module')
+    def api(self):
+        yield Api888Sport()
+
+    # TODO add test competition
+
+    @pytest.mark.parametrize('url', urls_888sport)
+    def test_odds(self, api, url):
+        odds = api.odds(url)
+        pprint(odds)
+        assert odds
 
 
 @pytest.mark.ApiBet365
@@ -66,9 +76,12 @@ class TestApiBet365(BaseTest):
     def api(self):
         yield ApiBet365()
 
+    # TODO add test competition
+
     @pytest.mark.parametrize('url', urls_bet365)
     def test_odds(self, api, url):
         odds = api.odds(url)
+        pprint(odds)
         assert odds
 
 
