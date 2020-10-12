54302c7538ad2921992cf756b40219b8933c618a
S1M0N38 <bertolottosimone@gmail.com>
2020-10-12 10:08:58 +0200

    move to international version of url

--- soccerapi/tests/urls.py
+++ soccerapi/tests/urls.py
@@ -1,19 +1,19 @@
 urls_bet365 = [
-    'https://www.bet365.it/#/AC/B1/C1/D13/E52224631/F2/',  # italy-seria_a
-    'https://www.bet365.it/#/AC/B1/C1/D13/E52547961/F2/I1/',  # italy-serie-b
-    'https://www.bet365.it/#/AC/B1/C1/D13/E51761579/F2/',  # england-premier_league
-    'https://www.bet365.it/#/AC/B1/C1/D13/E51791071/F2/I1/',  # england-championship
-    'https://www.bet365.it/#/AC/B1/C1/D13/E51669667/F2/I1/',  # germany-bundesliga
-    'https://www.bet365.it/#/AC/B1/C1/D13/E51669781/F2/I1/',  # germany-bundesliga2
+    'https://www.bet365.com/#/AC/B1/C1/D13/E52224631/F2/',  # italy-seria_a
+    'https://www.bet365.com/#/AC/B1/C1/D13/E52547961/F2/I1/',  # italy-serie-b
+    'https://www.bet365.com/#/AC/B1/C1/D13/E51761579/F2/',  # england-premier_league
+    'https://www.bet365.com/#/AC/B1/C1/D13/E51791071/F2/I1/',  # england-championship
+    'https://www.bet365.com/#/AC/B1/C1/D13/E51669667/F2/I1/',  # germany-bundesliga
+    'https://www.bet365.com/#/AC/B1/C1/D13/E51669781/F2/I1/',  # germany-bundesliga2
 ]
 
 urls_888sport = [
-    'https://www.888sport.it/#/filter/football/italy/serie_a',  # italy-seria_a
-    'https://www.888sport.it/#/filter/football/italy/serie_b',  # italy-serie-b
-    'https://www.888sport.it/#/filter/football/england/premier_league',  # england-premier_league
-    'https://www.888sport.it/#/filter/football/england/the_championship',  # england-championship
-    'https://www.888sport.it/#/filter/football/germany/bundesliga',  # germany-bundesliga
-    'https://www.888sport.it/#/filter/football/germany/2__bundesliga',  # germany-bundesliga2
+    'https://www.888sport.com/#/filter/football/italy/serie_a',  # italy-seria_a
+    'https://www.888sport.com/#/filter/football/italy/serie_b',  # italy-serie-b
+    'https://www.888sport.com/#/filter/football/england/premier_league',  # england-premier_league
+    'https://www.888sport.com/#/filter/football/england/the_championship',  # england-championship
+    'https://www.888sport.com/#/filter/football/germany/bundesliga',  # germany-bundesliga
+    'https://www.888sport.com/#/filter/football/germany/2__bundesliga',  # germany-bundesliga2
 ]
 
 urls_unibet = [
