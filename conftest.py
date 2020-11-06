b21eb619bb225e68d8dea653566d27613bc78dbb
S1M0N38 <bertolottosimone@gmail.com>
2020-11-06 15:52:34 +0100

    add conftest.py: --ip print information about current ip address

--- conftest.py
+++ conftest.py
@@ -1,18 +1,18 @@
-import os
+# import os
 
 import requests
 
 
 def pytest_addoption(parser):
-    parser.addoption("--proxy", action="store")
+    parser.addoption("--ip", action="store_true")
+    # parser.addoption("--proxy", action="store_true")
 
 
 def pytest_sessionstart(session):
-    proxy = session.config.option.proxy
-    if proxy:
-        os.environ['HTTPS_PROXY'] = proxy
-        print(f'HTTPS_PROXY={os.getenv("HTTPS_PROXY")}')
-    ip_info()
+    if session.config.option.ip:
+        ip_info()
+    # if session.config.option.proxy:
+    #     config_proxy()
 
 
 def ip_info():
@@ -23,3 +23,12 @@ def ip_info():
     print(f'hostname: {info["YourFuckingHostname"]}')
     print(f'ips: {info["YourFuckingISP"]}')
     print(f'country code: {info["YourFuckingCountryCode"]}')
+
+
+# def config_proxy():
+#     if os.getenv('HTTP_PROXY') is None:
+#         proxy = os.environ['PROXY']
+#         username = os.environ['PROXY_USERNAME']
+#         password = os.environ['PROXY_PASSWORD']
+#         os.environ['HTTPS_PROXY'] = f'http://{username}:{password}@{proxy}/'
+#     print('HTTPS_PROXY environment variable has been set')
