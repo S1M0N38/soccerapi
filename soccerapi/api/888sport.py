d733af64ffd27195e8f064f06e9469128b3857fe
S1M0N38 <bertolottosimone@gmail.com>
2021-01-17 16:50:50 +0100

    refactoring 888sport.py with new structure

--- soccerapi/api/888sport.py
+++ soccerapi/api/888sport.py
@@ -1,22 +1,18 @@
 import re
+from typing import Dict, Tuple
 
-from .base import ApiKambi
+import requests
+
+from .base import ApiBase
+from .kambi import ParserKambi as Parser888Sport
 
 
-class Api888Sport(ApiKambi):
+class Api888Sport(ApiBase, Parser888Sport):
     """ The ApiKambi implementation for 888sport.com """
 
     def __init__(self):
         self.name = '888sport'
-        self.base_url = (
-            'https://eu-offering.kambicdn.org/'
-            'offering/v2018/888/listView/football'
-        )
-        self.parsers = [
-            self._full_time_result,
-            self._both_teams_to_score,
-            self._double_chance,
-        ]
+        self.session = requests.Session()
 
     def competition(self, url: str) -> str:
         re_888sport = re.compile(
@@ -28,3 +24,29 @@ class Api888Sport(ApiKambi):
         else:
             msg = f'Cannot parse {url}'
             raise ValueError(msg)
+
+    def requests(self, competition: str) -> Tuple[Dict]:
+        return {
+            'full_time_result': self._request(competition, 12579),
+            'both_teams_to_score': self._request(competition, 11942),
+            'double_chance': self._request(competition, 12220),
+        }
+
+    # Auxiliary methods
+
+    def _request(
+        self, competition: str, category: int, market: str = 'IT'
+    ) -> Dict:
+        """ Make the single request using the active session """
+
+        base_url = (
+            'https://eu-offering.kambicdn.org/'
+            'offering/v2018/888/listView/football'
+        )
+        url = '/'.join([base_url, competition]) + '.json'
+        params = (
+            ('lang', 'en_US'),
+            ('market', market),
+            ('category', category),
+        )
+        return self.session.get(url, params=params).json()
