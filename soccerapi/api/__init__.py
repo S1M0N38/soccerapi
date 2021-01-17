524dedfa7824750836ad1cd6618931714645d9b1
S1M0N38 <bertolottosimone@gmail.com>
2021-01-17 17:06:42 +0100

    add example.py in __init__.py

--- soccerapi/api/__init__.py
+++ soccerapi/api/__init__.py
@@ -5,3 +5,5 @@ package = 'soccerapi.api'
 Api888Sport = importlib.import_module('.888sport', package).Api888Sport
 ApiBet365 = importlib.import_module('.bet365', package).ApiBet365
 ApiUnibet = importlib.import_module('.unibet', package).ApiUnibet
+
+# ApiExample = importlib.import_module('.example', package).ApiExample
