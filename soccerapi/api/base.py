b2774ad8286b9c13fe8a1128c2a734cdd3c9eef2
musso <francescomusso.main@gmail.com>
2021-01-25 00:54:55 +0100

    format odds in a event list

--- soccerapi/api/base.py
+++ soccerapi/api/base.py
@@ -3,7 +3,7 @@ from typing import List, Tuple
 
 
 class ApiBase(abc.ABC):
-    """ The Abstract Base Class on which every Api[Boolmaker] is based on. """
+    """ The Abstract Base Class on which every Api[Bookmaker] is based on. """
 
     @abc.abstractmethod
     def requests(self, competition: str, **kwargs) -> Tuple:
@@ -19,13 +19,24 @@ class ApiBase(abc.ABC):
 
     def odds(self, url: str) -> List:
         """ Get odds from url """
-        odds = self.requests(self.competition(url))
+        to_parse = self.requests(self.competition(url))
 
         # parse odds
-        for parser, data in odds.items():
-            odds[parser] = getattr(self, parser)(data)
+        to_format = dict()
+        for parser, data in to_parse.items():
+            to_format[parser]= getattr(self, parser)(data) # why not "self.parser(data)" ??
 
-        # TODO merge odds
+        # TODO format odds
+        # get list of events
+        odds = to_format[list(to_parse.keys())[0]].copy()
+        for ind, event in enumerate(odds):
+            for parser in to_parse.keys():
+                # add market (parser) to event
+                event[parser] = to_format[parser][ind].get('odds')
+            # pop inherited odds key
+            event.pop('odds')
+            odds[ind] = event.copy()
+
         return odds
 
 
